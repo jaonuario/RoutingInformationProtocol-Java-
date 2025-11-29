f2ba1012a70d25bd2f5b59583a9aedb7637703b6
João Pedro Januario <jpedrojanus@hotmail.com>
2025-11-29 17:19:47 -0300

    add file RoutingInformationProtocol.java

---
+++ src/rip/RoutingInformationProtocol.java
@@ -0,0 +1,32 @@
+package rip;
+
+import interfaces.RoutingProtocolManagementInterface;
+import interfaces.UnicastServiceInterface;
+
+public class RoutingInformationProtocol implements UnicastServiceInterface, RoutingProtocolManagementInterface {
+
+    @Override
+    public void UPDataReq(short destinationEntity, String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'UPDataReq'");
+    }
+
+    @Override
+    public boolean getDistanceTable(short node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDistanceTable'");
+    }
+
+    @Override
+    public boolean getLinkCost(short nodeA, short nodeB) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLinkCost'");
+    }
+
+    @Override
+    public boolean setLinkCost(short nodeA, short nodeB, int cost) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setLinkCost'");
+    }
+    
+}
